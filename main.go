ad3f76f78ce1ae925a0507eb69b557498b6907a0
Michal Dziurowski <mdziurowski@gmail.com>
2019-06-10 21:09:01 +0200

    faster time parse

--- main.go
+++ main.go
@@ -5,12 +5,12 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 )
 
 func main() {
-	report(os.Args[1], os.Args[2])
+	// report(os.Args[1], os.Args[2])
+	report("data.txt", "summary.txt")
 }
 
 func report(in, out string) {
@@ -21,7 +21,7 @@ func report(in, out string) {
 	scanner := bufio.NewScanner(inFile)
 
 	for scanner.Scan() {
-		record := newCarRecord(scanner.Text())
+		record := newCarRecord(scanner.Bytes())
 		duration := record.End.Sub(record.Start).Seconds()
 		if _, ok := durations[record.ID]; ok {
 			durations[record.ID] += duration
@@ -50,12 +50,29 @@ type carRecord struct {
 	ID    int
 }
 
-func newCarRecord(line string) carRecord {
-	parts := strings.Split(line, " ")
+func newCarRecord(b []byte) carRecord {
 
-	start, _ := time.Parse("2006-01-02T15:04:05", parts[0])
-	end, _ := time.Parse("2006-01-02T15:04:05", parts[1])
-	id, _ := strconv.Atoi(parts[2])
+	start := parseTime(b[:19])
+	end := parseTime(b[20:39])
+	id, _ := strconv.Atoi(string(b[40:48]))
 
 	return carRecord{start, end, id}
+}
+
+func parseTime(b []byte) time.Time {
+	y := from4digit(b[:4])
+	m := time.Month(from2digit(b[5:7]))
+	d := from2digit(b[8:10])
+	h := from2digit(b[11:13])
+	mi := from2digit(b[14:16])
+	s := from2digit(b[17:19])
+	return time.Date(y, m, d, h, mi, s, 0, time.UTC)
+}
+
+func from4digit(b []byte) int {
+	return int(b[0]-'0')*1000 + int(b[1]-'0')*100 + int(b[2]-'0')*10 + int(b[3]-'0')
+}
+
+func from2digit(b []byte) int {
+	return int(b[0]-'0')*10 + int(b[1]-'0')
 }
