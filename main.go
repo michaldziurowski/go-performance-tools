f3410f5231f5f075b444df5d39413eaa7b597c21
Michal Dziurowski <mdziurowski@gmail.com>
2019-07-15 22:28:23 +0200

    concurrency but with errors

--- main.go
+++ main.go
@@ -23,23 +23,52 @@ func report(in, out string) {
 	wg := &sync.WaitGroup{}
 	mu := &sync.Mutex{}
 
-	for _, line := range lines {
-		wg.Add(1)
-		go (func() {
-			if line != "" {
-				record := newCarRecord(line)
-				duration := record.End.Sub(record.Start).Seconds()
-				mu.Lock()
-				durations[record.ID] += duration
-				mu.Unlock()
+	noOfWorkers := 4
+	cWork := make(chan []string, 100)
+
+	for i := 0; i < noOfWorkers; i++ {
+		go func() {
+			for l := range cWork {
+				for i := 0; i < len(l); i++ {
+					line := l[i]
+					if line != "" {
+						record := newCarRecord(line)
+						duration := record.End.Sub(record.Start).Seconds()
+						mu.Lock()
+						durations[record.ID] += duration
+						mu.Unlock()
+					}
+				}
 			}
+
 			wg.Done()
-		})()
+		}()
 	}
+
+	wg.Add(noOfWorkers)
+
+	linesSlice := make([]string, 100)
+	counter := 0
+
+	for _, line := range lines {
+		if counter == 100 {
+			cWork <- linesSlice
+			counter = 0
+		}
+		linesSlice[counter] = line
+		counter++
+	}
+
+	if counter != 0 {
+		cWork <- linesSlice[0:counter]
+	}
+
+	close(cWork)
 
 	wg.Wait()
 
 	var sb strings.Builder
+
 	for id, duration := range durations {
 		sb.WriteString(fmt.Sprintf("%d %.0f\r\n", id, duration))
 	}
