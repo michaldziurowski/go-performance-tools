0e2f9c2805651893c65a8304b3daab3fbe82ae78
Michal Dziurowski <mdziurowski@gmail.com>
2019-08-11 18:36:54 +0200

    pretty twaks

--- main.go
+++ main.go
@@ -20,47 +20,43 @@ func report(in, out string) {
 	defer inFile.Close()
 
 	durations := make(map[int]float64)
-	var wg sync.WaitGroup
 
+	lineLen := 50
+	dataInLineLen := 48 // dont count \r\n
 	noOfWorkers := 4
 	cWork := make(chan []byte, 100)
 	cDurations := make(chan map[int]float64, noOfWorkers)
+	wg := sync.WaitGroup{}
 
 	for i := 0; i < noOfWorkers; i++ {
 		go func() {
-			locdurations := make(map[int]float64)
-			for l := range cWork {
-				for x := 0; x < len(l); x += 50 {
-					line := string(l[x : x+48]) // 48 because we dont want to add \r\n
-					if line != "" {
-						record := newCarRecord(line)
-						duration := record.End.Sub(record.Start).Seconds()
-						locdurations[record.ID] += duration
-					} else {
-						fmt.Printf("Received empty line\n")
-					}
+			localDurations := make(map[int]float64)
+			for dataRange := range cWork {
+				for x := 0; x < len(dataRange); x += lineLen {
+					line := string(dataRange[x : x+dataInLineLen])
+					record := newCarRecord(line)
+					duration := record.End.Sub(record.Start).Seconds()
+					localDurations[record.ID] += duration
 				}
 			}
-			cDurations <- locdurations
+			cDurations <- localDurations
 			wg.Done()
 		}()
 	}
 
 	wg.Add(noOfWorkers)
 
-	divider := 100000
-	readCount := 0
+	batchSize := 100000
 
 	for {
-		buf := make([]byte, divider)
-
+		buf := make([]byte, batchSize)
 		read, err := inFile.Read(buf)
+
 		if err == io.EOF {
 			break
 		}
 
 		cWork <- buf[:read]
-		readCount += read
 	}
 
 	close(cWork)
@@ -75,7 +71,6 @@ func report(in, out string) {
 	}
 
 	var sb strings.Builder
-	fmt.Printf("durations : %v\n", len(durations))
 	for id, duration := range durations {
 		sb.WriteString(fmt.Sprintf("%d %.0f\r\n", id, duration))
 	}
