3557743e73da4cbae0eaf5f9f358f00763725e45
Michal Dziurowski <mdziurowski@gmail.com>
2019-08-11 20:17:13 +0200

    pretty tweaks

--- main.go
+++ main.go
@@ -14,32 +14,27 @@ func main() {
 	report(os.Args[1], os.Args[2])
 }
 
-func report(in, out string) {
-	bytes, _ := ioutil.ReadFile(in)
+func report(inFileName, outFileName string) {
+	bytes, _ := ioutil.ReadFile(inFileName)
 	content := string(bytes)
 	lines := strings.Split(content, "\r\n")
 
 	durations := make(map[int]float64)
-	var wg sync.WaitGroup
-	var mu sync.Mutex
-
+	wg := sync.WaitGroup{}
+	mu := sync.Mutex{}
 	noOfWorkers := 4
 	cWork := make(chan []string, 100)
 
 	for i := 0; i < noOfWorkers; i++ {
 		go func() {
-			for l := range cWork {
-				for x := 0; x < len(l); x++ {
-					line := l[x]
-					if line != "" {
-						record := newCarRecord(line)
-						duration := record.End.Sub(record.Start).Seconds()
-						mu.Lock()
-						durations[record.ID] += duration
-						mu.Unlock()
-					} else {
-						fmt.Printf("Received empty line\n")
-					}
+			for dataBatch := range cWork {
+				for x := 0; x < len(dataBatch); x++ {
+					line := dataBatch[x]
+					record := newCarRecord(line)
+					duration := record.End.Sub(record.Start).Seconds()
+					mu.Lock()
+					durations[record.ID] += duration
+					mu.Unlock()
 				}
 			}
 
@@ -49,27 +44,29 @@ func report(in, out string) {
 
 	wg.Add(noOfWorkers)
 
-	linesLen := len(lines)
-	divider := 100000
-	times := int(linesLen / divider)
+	noOfLines := len(lines) - 1 // because we know that the last line is empty
+	batchSize := 100000
+	noOfBatches := int(noOfLines / batchSize)
 
-	for y := 0; y < times; y++ {
-		cWork <- lines[y*divider : (y*divider)+divider]
+	for x := 0; x < noOfBatches; x++ {
+		startAt := x * batchSize
+		cWork <- lines[startAt : startAt+batchSize]
 	}
 
-	cWork <- lines[times*divider:]
+	if noOfBatches*batchSize < noOfLines {
+		cWork <- lines[noOfBatches*batchSize : noOfLines]
+	}
 
 	close(cWork)
 
 	wg.Wait()
 
 	var sb strings.Builder
-	fmt.Printf("durations : %v\n", len(durations))
 	for id, duration := range durations {
 		sb.WriteString(fmt.Sprintf("%d %.0f\r\n", id, duration))
 	}
 
-	ioutil.WriteFile(out, []byte(sb.String()), 0644)
+	ioutil.WriteFile(outFileName, []byte(sb.String()), 0644)
 }
 
 type carRecord struct {
